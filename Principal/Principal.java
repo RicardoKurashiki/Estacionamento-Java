95443b669ac33b8312df35a08c49e10883c15dd4
RicardoKurashiki <ricardo.g.kurashiki@gmail.com>
2021-05-05 18:11:14 -0300

    Pesquisa por marca e menu de saida

--- Principal/Principal.java
+++ Principal/Principal.java
@@ -14,13 +14,52 @@ public class Principal {
 
 	public static void main(String[] args) {
 
+		// -> Dummies para exemplos
+		final Marca m1 = new Marca("Fiat");
+		final Marca m2 = new Marca("Renault");
+		final Marca m3 = new Marca("Chevrolet");
+
+		m1.addModelo("Uno");
+		m1.addModelo("Argo");
+		m2.addModelo("Kwid");
+		m2.addModelo("Duster");
+		m3.addModelo("Camaro");
+		m3.addModelo("Cruze");
+
+		marcas.add(m1);
+		marcas.add(m2);
+		marcas.add(m3);
+
+		// -> Criacao de variaveis
 		boolean appOn = true;
 		Scanner scanner = new Scanner(System.in);
 
+		// -> Loop do menu
 		while (appOn) {
 			int escolha = menu();
 			switch (escolha) {
+				case 1:
+					// -> Entrada do carro
+					appOn = false;
+					break;
+				case 2:
+					// -> Saida do carro
+					appOn = false;
+					break;
+				case 3:
+					// -> Nova marca
+					appOn = false;
+					break;
+				case 4:
+					// -> Novo modelo
+					appOn = false;
+					break;
+				case 5:
+					// -> Historico
+					mostrarMarcas();
+					break;
 				case 0:
+					// ->Sair do menu
 					appOn = false;
 					break;
 				default:
@@ -31,23 +70,27 @@ public class Principal {
 		scanner.close();
 	}
 
-	private static void entradaCarro() {
-	}
-
-	private static float saidaCarro() {
-		float preco = 0;
-		return preco;
-	}
-
 	public static int menu() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("\n--------------------");
-		System.out.println("<1> Cadastrar entrada de um carro");
+		System.out.println("<1> Registrar entrada de um carro");
 		System.out.println("<2> Registrar saida de um carro");
-		System.out.println("<3> Ver histórico de movimentacoes");
+		System.out.println("<3> Ver/Registrar marcas de carro");
+		System.out.println("<4> Ver/Registrar novo modelo de carro");
+		System.out.println("<5> Ver historico de movimentacoes");
 		System.out.println("<0> Sair do aplicativo");
 		System.out.println("--------------------");
 		System.out.print(">> ");
 		return scanner.nextInt();
+	}
+
+	public static void mostrarMarcas() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("\n--------------------");
+		System.out.println("As marcas disponiveis no sistema sao:");
+		for (Marca marca : marcas) {
+			System.out.println("<" + marcas.indexOf(marca) + "> " + marca.getMarca());
+		}
+		int opcao = scanner.nextInt() + 1;
 	}
 }
