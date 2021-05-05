8edac0da61bb04c05252619cb1dc2967cd32c7af
Carlos Eduardo Marques Assunção Torres <65908990+carlosqas@users.noreply.github.com>
2021-05-05 15:23:31 -0300

    dasfdasudias

--- Modelagem/Carro.java
+++ Modelagem/Carro.java
@@ -10,19 +10,65 @@ public class Carro {
 	private LocalDateTime saida;
 	private float valor;
 
-	// -> Construtor para entrada de carro
-	public Carro(Modelo modelo, String placa, LocalDateTime entrada) {
-		this.modelo = modelo;
+	// -> Construtor apenas com placa
+	public Carro(String placa){
 		this.placa = placa;
+		this.modelo = new Modelo();
+		this.entrada = LocalDateTime.now();
+	}
+
+	// -> Construtor com o nome do modelo e a placa;
+	public Carro(String modelo, String placa){
+		this.placa = placa;
+		this.modelo = new Modelo(modelo);
+		this.entrada = LocalDateTime.now();
+	}
+
+	// -> Construtor com o objeto modelo e com a placa
+	public Carro(Modelo modelo, String placa){
+		this.placa = placa;
+		this.modelo = modelo;
+		this.entrada = LocalDateTime.now();
+	}
+
+	// -> Construtor com o nome do modelo, placa e instância da data de entrada
+	public Carro(String modelo, String placa, LocalDateTime entrada){
+		this.placa = placa;
+		this.modelo = new Modelo(modelo);
 		this.entrada = entrada;
 	}
 
-	// -> Construtor para saida de carro
-	public Carro(Modelo modelo, String placa, LocalDateTime saida, float valor) {
-		this.modelo = modelo;
+	// -> Construtor com o nome do modelo, placa e informações da hora de entrada
+	public Carro(String modelo, String placa, int hora, int minuto, int dia, int mes, int ano){
 		this.placa = placa;
-		this.saida = saida;
-		this.valor = valor;
+		this.modelo = new Modelo(modelo);
+		this.entrada = LocalDateTime.of(ano, mes, dia, hora, minuto);
+	}
+
+	// -> Construtor com o objeto modelo, placa e instância da hora de entrada
+	public Carro(Modelo modelo, String placa, LocalDateTime entrada){
+		this.placa = placa;
+		this.modelo = modelo;
+		this.entrada = entrada;
+	}
+
+	// -> Construtor com o objeto modelo, placa e informações da hora de entrada
+	public Carro(Modelo modelo, String placa, int hora, int minuto, int dia, int mes, int ano){
+		this.placa = placa;
+		this.modelo = modelo;
+		this.entrada = LocalDateTime.of(ano, mes, dia, hora, minuto);
+	}
+
+
+
+	public float saidaCarro(){
+		// Estabelece o LocalDateTime saída como now()
+		// Calcula valor com a diferença entre o tempo de registro e o tempo de saida
+	}
+
+	public float saidaCarroHorario(){
+		// Estabelece o LocalDateTime saída com a data e horário passado pelo usuário
+		// Calcula valor com a diferença de entrada e saída;
 	}
 
 }
--- Modelagem/Modelo.java
+++ Modelagem/Modelo.java
@@ -3,19 +3,19 @@ package Modelagem;
 public class Modelo {
 	private String nome;
 
-	// Construtor do modelo sem inputs
+	// -> Construtor do modelo sem inputs
 	public Modelo(){
 		nome = "Sem modelo";
 	}
-	// Construtor do modelo com nome
+	// -> Construtor do modelo com nome
 	public Modelo(String nome){
 		this.nome = nome;
 	}
-	// Método para mudar ou colocar nome em uma marca
+	// -> Método para mudar ou colocar nome em uma marca
 	public void setModelo(String nome){
 		this.nome = nome;
 	}
-	// Método para retornar nome do modelo
+	// -> Método para retornar nome do modelo
 	public String getModelo (){
 		return nome;
 	}
