b1ec623081dadff610f916893bb1e42792edd071
Carlos Eduardo Marques Assunção Torres <65908990+carlosqas@users.noreply.github.com>
2021-05-05 14:36:50 -0300

    Finalização de Marca e Modelo

--- Modelagem/Marca.java
+++ Modelagem/Marca.java
@@ -7,28 +7,34 @@ public class Marca {
 	private String nome;
 	private ArrayList<Modelo> modelo;
 
+	// Método construtor sem nome
 	public Marca(){
 		nome = "Sem marca";
 		modelo = new ArrayList<Modelo>();
 	}
 
+	// Método construtor com nome
 	public Marca(String nome){
 		this.nome = nome;
 		modelo = new ArrayList<Modelo>();
 	}
 
+	// Colocar ou mudar o nome de uma marca
 	public void setMarca(String nome){
 		this.nome = nome;
 	}
 
+	// Retorna nome da marca
 	public String getMarca(){
 		return nome;
 	}
 
-	public String getModelo(){
-		return "Ainda não sei o que fazer aqui";
+	// Retorna o objeto da classe modelo de acordo com o índice
+	public Modelo getModelo(int index){
+		return modelo.get(index);
 	}
 
+	// Adiciona modelos na marca
 	public void addModelo(String nomeMod){
 		Modelo novoMod = new Modelo(nomeMod);
 		modelo.add(novoMod);
