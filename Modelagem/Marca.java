791df36f0c76adab77facd8665cbd851e672cf11
Carlos Eduardo Marques Assunção Torres <65908990+carlosqas@users.noreply.github.com>
2021-05-05 14:32:00 -0300

    Adicionado métodos construtores, getters e setters (Marca e Modelo)

--- Modelagem/Marca.java
+++ Modelagem/Marca.java
@@ -7,5 +7,30 @@ public class Marca {
 	private String nome;
 	private ArrayList<Modelo> modelo;
 
+	public Marca(){
+		nome = "Sem marca";
+		modelo = new ArrayList<Modelo>();
+	}
 
+	public Marca(String nome){
+		this.nome = nome;
+		modelo = new ArrayList<Modelo>();
+	}
+
+	public void setMarca(String nome){
+		this.nome = nome;
+	}
+
+	public String getMarca(){
+		return nome;
+	}
+
+	public String getModelo(){
+		return "Ainda não sei o que fazer aqui";
+	}
+
+	public void addModelo(String nomeMod){
+		Modelo novoMod = new Modelo(nomeMod);
+		modelo.add(novoMod);
+	}
 }
